0b042cec82d48bce959670064bf973a6b41c1e0b
Arushi Singhal <arushisinghal19971997@gmail.com>
2019-03-22 17:28:46 +0530

    dummy code

--- dummy_code.py
+++ dummy_code.py
@@ -4,27 +4,37 @@ from pickle import dump
 from unicodedata import normalize
 from numpy import array
 
-# load doc into memory
 def load_doc(filename):
-	# open the file as read only
-	file = open(filename, mode='rt', encoding='utf-8')
-	# read all text
-	text = file.read()
-	# close the file
-	file.close()
-	return text
+    file = open(filename, mode='rt', encoding='utf-8')
+    text = file.read()
+    file.close()
+    return text
 
-# split a loaded document into sentences
-def to_pairs(doc):
-    lines = doc.strip().split('\n')
-    print (lines)
-    print (type(lines))
-    pairs = [line.split('\t') for line in  lines]
+def to_pairs(english_text, hindi_text):
+    english_lines = english_text.strip().split('\n')
+    hindi_lines = hindi_text.strip().split('\n')
+    pairs = []
+    for i in range(len(hindi_lines)):
+        pairs.append([])
+        pairs[i].append(pre_process_english_sentence(english_lines[i]))
+        pairs[i].append(hindi_lines[i])
     return pairs
 
-filename = 'deu.txt'
-doc = load_doc(filename)
-# split into english-german pairs
-pairs = to_pairs(doc)
-#print (pairs)
-print (type(pairs))
+def pre_process_english_sentence(line):
+    re_print = re.compile('[^%s]' % re.escape(string.printable))
+    table = str.maketrans('', '', string.punctuation)
+    line = normalize('NFD', line).encode('ascii', 'ignore')
+    line = line.decode('UTF-8')
+    line = line.split()
+    line = [word.lower() for word in line]
+    line = [word.translate(table) for word in line]
+    line = [re_print.sub('', w) for w in line]
+    line = [word for word in line if word.isalpha()]
+    line = ' '.join(line)
+    return line
+
+english_text = load_doc('English')
+hindi_text = load_doc('Hindi')
+#hindi_text = load_doc('French.txt')
+pairs = to_pairs(english_text, hindi_text)
+print (pairs)
