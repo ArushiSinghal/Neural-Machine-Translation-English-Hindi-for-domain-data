ceede7b6ee8a7ce1c74f6c32ac4bd0c3ef84df6d
Arushi Singhal <arushisinghal19971997@gmail.com>
2019-03-23 20:57:04 +0530

    added disctionary for one-hot vector

--- dummy_code.py
+++ dummy_code.py
@@ -1,8 +1,21 @@
+#from __future__ import unicode_literals, print_function, division
+import numpy
+import scipy
+import matplotlib
+import pandas
+import statsmodels
+import sklearn
+import tensorflow
+import keras
+from io import open
+import unicodedata
 import string
 import re
-from pickle import dump
-from unicodedata import normalize
-from numpy import array
+import random
+import torch
+import torch.nn as nn
+from torch import optim
+import torch.nn.functional as F
 INDIC_NLP_LIB_HOME=r"/home/arushi/Neural-Machine-Translation/anoopkunchukuttan-indic_nlp_library-eccde81/src"
 INDIC_NLP_RESOURCES=r"/home/arushi/Neural-Machine-Translation/indic_nlp_resources-master"
 import sys
@@ -13,7 +26,37 @@ from indicnlp import loader
 loader.load()
 from indicnlp.normalize.indic_normalize import IndicNormalizerFactory
 from indicnlp.tokenize import indic_tokenize
+from pickle import dump
+from unicodedata import normalize
+from numpy import array
 
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+SOS_token = 0
+EOS_token = 1
+
+class Lang:
+    def __init__(self, name):
+        self.name = name
+        #index of each new word
+        self.word2index = {}
+        #count of that word
+        self.word2count = {}
+        #index with word
+        self.index2word = {0: "SOS", 1: "EOS"}
+        self.n_words = 2  # Count SOS and EOS
+    def addSentence(self, sentence):
+        for word in sentence.split(' '):
+            self.addWord(word)
+    def addWord(self, word):
+        if word not in self.word2index:
+            self.word2index[word] = self.n_words
+            self.word2count[word] = 1
+            self.index2word[self.n_words] = word
+            self.n_words += 1
+        else:
+            self.word2count[word] += 1
 
 def load_doc(filename):
     file = open(filename, mode='rt', encoding='utf-8')
@@ -80,9 +123,26 @@ def pre_process_hindi_sentence(line):
     line = ' '.join(line)
     return (line)
 
+def prepareData(pairs):
+    input_lang = Lang('eng')
+    output_lang = Lang('hin')
+    for pair in pairs:
+        input_lang.addSentence(pair[0])
+        output_lang.addSentence(pair[1])
+    print("Counted words:")
+    print(input_lang.name, input_lang.word2index)
+    print(input_lang.name, input_lang.word2count)
+    print(input_lang.name, input_lang.index2word)
+    print(input_lang.name, input_lang.n_words)
+    print(output_lang.name, output_lang.word2index)
+    print(output_lang.name, output_lang.word2count)
+    print(output_lang.name, output_lang.index2word)
+    print(output_lang.name, output_lang.n_words)
+    return input_lang, output_lang, pairs
 
 english_text = load_doc('English')
 hindi_text = load_doc('Hindi')
 #hindi_text = load_doc('French.txt')
 pairs = to_pairs(english_text, hindi_text)
+input_lang, output_lang, pairs = prepareData(pairs)
 #print (pairs)
