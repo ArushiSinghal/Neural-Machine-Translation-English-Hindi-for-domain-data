57c05c3b3b3b5b27b99930a02d0ceb81123a9302
Arushi Singhal <arushisinghal19971997@gmail.com>
2019-03-26 22:19:47 +0530

    pytorch starting

---
+++ code_pytorch_new_trial.py
@@ -0,0 +1,192 @@
+from __future__ import unicode_literals, print_function, division
+#from __future__ import unicode_literals, print_function, division
+import numpy
+import scipy
+import matplotlib
+import pandas
+import statsmodels
+import sklearn
+import tensorflow
+import keras
+from io import open
+import unicodedata
+import string
+import re
+import random
+import torch
+import torch.nn as nn
+from torch import optim
+import torch.nn.functional as F
+from pickle import load
+from pickle import dump
+from numpy.random import rand
+from numpy.random import shuffle
+import string
+import re
+from pickle import dump
+from unicodedata import normalize
+from numpy import array
+from pickle import load
+from numpy import array
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.utils import to_categorical
+from keras.utils.vis_utils import plot_model
+from keras.models import Sequential
+from keras.layers import LSTM
+from keras.layers import Dense
+from keras.layers import Embedding
+from keras.layers import RepeatVector
+from keras.layers import TimeDistributed
+from keras.callbacks import ModelCheckpoint
+INDIC_NLP_LIB_HOME=r"/home/arushi/Neural-Machine-Translation/anoopkunchukuttan-indic_nlp_library-eccde81/src"
+INDIC_NLP_RESOURCES=r"/home/arushi/Neural-Machine-Translation/indic_nlp_resources-master"
+import sys
+sys.path.append(r'{}'.format(INDIC_NLP_LIB_HOME))
+from indicnlp import common
+common.set_resources_path(INDIC_NLP_RESOURCES)
+from indicnlp import loader
+loader.load()
+from indicnlp.normalize.indic_normalize import IndicNormalizerFactory
+from indicnlp.tokenize import indic_tokenize
+from pickle import dump
+from unicodedata import normalize
+from numpy import array
+from io import open
+import unicodedata
+import string
+import re
+import random
+
+import torch
+import torch.nn as nn
+from torch import optim
+import torch.nn.functional as F
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+SOS_token = 0
+EOS_token = 1
+
+#tokenizing data start
+
+class Lang:
+    def __init__(self, name):
+        self.name = name
+        #index of each new word
+        self.word2index = {}
+        #count of that word
+        self.word2count = {}
+        #index with word
+        self.index2word = {0: "SOS", 1: "EOS"}
+        self.n_words = 2  # Count SOS and EOS
+    def addSentence(self, sentence):
+        for word in sentence.split(' '):
+            self.addWord(word)
+    def addWord(self, word):
+        if word not in self.word2index:
+            self.word2index[word] = self.n_words
+            self.word2count[word] = 1
+            self.index2word[self.n_words] = word
+            self.n_words += 1
+        else:
+            self.word2count[word] += 1
+
+def load_doc(filename):
+    file = open(filename, mode='rt', encoding='utf-8')
+    text = file.read()
+    file.close()
+    return text
+
+def to_pairs(english_text, hindi_text):
+    english_lines = english_text.strip().split('\n')
+    hindi_lines = hindi_text.strip().split('\n')
+    pairs = []
+    for i in range(len(hindi_lines)):
+        pairs.append([])
+        pairs[i].append(pre_process_english_sentence(english_lines[i]))
+        pairs[i].append(pre_process_hindi_sentence(hindi_lines[i]))
+    return pairs
+
+
+def clean_text(line):
+    text = line
+    text=text.replace(u',','')
+    text=text.replace(u'"','')
+    text=text.replace(u'(','')
+    text=text.replace(u')','')
+    text=text.replace(u'"','')
+    text=text.replace(u':','')
+    text=text.replace(u"'",'')
+    text=text.replace(u"‘‘",'')
+    text=text.replace(u"’’",'')
+    text=text.replace(u"''",'')
+    text=text.replace(u".",'')
+    text=text.replace(u"-",'')
+    text=text.replace(u"।",'')
+    text=text.replace(u"?",'')
+    text=text.replace(u"\\",'')
+    text=text.replace(u"_",'')
+    text=re.sub('[a-zA-Z]', '', text)
+    text=re.sub('[0-9+\-*/.%]', '', text)
+    return text
+
+def pre_process_english_sentence(line):
+    re_print = re.compile('[^%s]' % re.escape(string.printable))
+    table = str.maketrans('', '', string.punctuation)
+    line = normalize('NFD', line).encode('ascii', 'ignore')
+    line = line.decode('UTF-8')
+    line = line.split()
+    line = [word.lower() for word in line]
+    line = [word.translate(table) for word in line]
+    line = [re_print.sub('', w) for w in line]
+    line = [word for word in line if word.isalpha()]
+    line = ' '.join(line)
+    return line
+
+def pre_process_hindi_sentence(line):
+    remove_nuktas=False
+    factory=IndicNormalizerFactory()
+    normalizer=factory.get_normalizer("hi",remove_nuktas)
+    line=normalizer.normalize(line)
+    line=clean_text(line)
+    tokens = list()
+    for t in indic_tokenize.trivial_tokenize(line):
+        tokens.append(t)
+    line = tokens
+    line = [word.lower() for word in line]
+    line = [word for word in line if not re.search(r'\d', word)]
+    line = ' '.join(line)
+    return (line)
+
+def prepareData(pairs):
+    input_lang = Lang('eng')
+    output_lang = Lang('hin')
+    for pair in pairs:
+        input_lang.addSentence(pair[0])
+        output_lang.addSentence(pair[1])
+    return input_lang, output_lang
+
+english_text = load_doc('English')
+hindi_text = load_doc('Hindi')
+pairs = to_pairs(english_text, hindi_text)
+input_lang, output_lang = prepareData(pairs)
+
+def indexesFromSentence(lang, sentence):
+    return [lang.word2index[word] for word in sentence.split(' ')]
+
+
+def tensorFromSentence(lang, sentence):
+    indexes = indexesFromSentence(lang, sentence)
+    indexes.append(EOS_token)
+    return torch.tensor(indexes, dtype=torch.long, device=device).view(-1, 1)
+
+
+def tensorsFromPair(pair):
+    print (pair)
+    input_tensor = tensorFromSentence(input_lang, pair[0])
+    target_tensor = tensorFromSentence(output_lang, pair[1])
+    return (input_tensor, target_tensor)
+
+training_pairs = [tensorsFromPair(random.choice(pairs)) for i in range(2)]
+print (training_pairs)
